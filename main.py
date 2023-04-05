08f68c2dd014feb118f3b849c086ce03937c0c23
StillFantasyC <111132969+StillFantasyC@users.noreply.github.com>
2023-04-06 06:50:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  wordDay = "今天是2023年4月5日，星期三。\n早上好呀宝儿，今天是清明节，好好休息嗷，爱你么么哒。\n"
+  wordDay = "今天是2023年4月6日，星期四。\n早上好呀宝儿，以后我会更加用心的和你交流，多多关心你，多多体贴你，晚上睡着的坏习惯也在逐步改正了呀宝儿，以后保证会更好呀，爱你么么哒。\n"
   if words.status_code != 200:
     return wordDay+get_words()
   return wordDay+words.json()['data']['text']
